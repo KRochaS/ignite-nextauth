27cc3851b50b01172ee6fb7a34e3394ea4e84125
KRochaS <karinerocha.s@hotmail.com>
2023-11-08 20:09:32 -0300

    feat: Components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,16 @@
 import { ComponentProps } from "react";
 
-type ButtonProps = ComponentProps<"button">;
+type ButtonProps = ComponentProps<"button"> & {
+  heightValue: number;
+};
 
-export function Button(props: ButtonProps) {
+export function Button({ heightValue, ...props }: ButtonProps) {
   return (
     <button
-      className="mt-2 h-12 w-72 rounded 
-                    bg-gradient-to-r from-orange-100
-                    to-purple-300 text-base
-                    font-semibold text-white"
+      className={`w-${heightValue} mt-2 h-12 rounded
+            bg-gradient-to-r from-orange-100
+            to-purple-300 p-1
+            text-base font-semibold`}
       {...props}
     />
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,11 +7,8 @@ type InputProps = ComponentProps<"input"> & {
 export function Input({ label, ...props }: InputProps) {
   return (
     <>
-      <label className="mb-2 text-base font-semibold text-white">{label}</label>
-      <input
-        className="mb-4 h-12 w-72 rounded  bg-gray-960 pl-2 text-white"
-        {...props}
-      />
+      <label className="mb-2 text-base font-semibold">{label}</label>
+      <input className="mb-4 h-12 w-72 rounded  bg-gray-960 pl-2" {...props} />
     </>
   );
 }
